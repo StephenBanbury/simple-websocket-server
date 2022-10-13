96158b3f1b998478cfa422497d718250586acf8c
Stephen Banbury <sbanbury@gmail.com>
2022-10-13 09:51:54 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,34 @@
+﻿const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({port: 8080}, () => {
+    console.log('server started');
+})
+
+wss.on('connection', function connection(ws) {
+    
+    ws.send('WebSocket connected');
+    
+    wss.broadcast = function(data, sender) {
+        wss.clients.forEach(function(client) {
+            if (client !== sender) {
+                client.send(data.toString())
+            } 
+        })
+    }
+    ws.on('message', (data) => {
+        console.log('data received: ', data.toString());
+        wss.broadcast(data, ws);
+    });
+    ws.on('error', error => {
+        console.log('exception: ', error.toString());
+        // TODO
+    });
+    ws.on('close', ws =>  {
+        console.log('close connection');
+        // TODO
+    });
+})
+
+wss.on('listening', () => {
+    console.log('listening on 8080');
+})
